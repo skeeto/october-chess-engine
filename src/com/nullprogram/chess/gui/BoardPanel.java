7c09c2e355e865ca9f7c905b5090356b77d2c102
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-05 21:27:46 -0400

    Added mouse selection to the panel.

--- src/com/nullprogram/chess/gui/BoardPanel.java
+++ src/com/nullprogram/chess/gui/BoardPanel.java
@@ -3,21 +3,26 @@ package com.nullprogram.chess.gui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 import javax.swing.JPanel;
 
 import com.nullprogram.chess.Board;
 import com.nullprogram.chess.Piece;
 import com.nullprogram.chess.Position;
 
-public class BoardPanel extends JPanel {
+public class BoardPanel extends JPanel implements MouseListener {
 
     private static final long serialVersionUID = 1L;
 
     private Board board;
+    private Position selected = null;
 
     private Color dark = new Color(0xC0, 0x56, 0x0);
     private Color light = new Color(0xFF, 0xA8, 0x58);
+    private Color selColor = new Color(0x00, 0xFF, 0xFF);
     static final int MIN_SIZE = 25;
     static final int PREF_SIZE = 50;
 
@@ -27,15 +32,22 @@ public class BoardPanel extends JPanel {
                                        PREF_SIZE * board.getHeight()));
         setMinimumSize(new Dimension(MIN_SIZE * board.getWidth(),
                                      MIN_SIZE * board.getHeight()));
+        addMouseListener(this);
+    }
+
+    private int getTileSize() {
+        int h = board.getHeight();
+        int w = board.getWidth();
+        int sizeX = getWidth() / w;
+        int sizeY = getHeight() / h;
+        return Math.min(sizeX, sizeY);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         int h = board.getHeight();
         int w = board.getWidth();
-        int sizeX = getWidth() / w;
-        int sizeY = getHeight() / h;
-        int size = Math.min(sizeX, sizeY);
+        int size = getTileSize();
 
         // Draw the background
         for (int y = 0; y < h; y++) {
@@ -59,5 +71,44 @@ public class BoardPanel extends JPanel {
                 }
             }
         }
+
+        // Draw selected square
+        if (selected != null) {
+            g.setColor(selColor);
+            int x = selected.x * size;
+            int y = (board.getHeight() - 1 - selected.y) * size;
+            int thickness = 3;
+            for (int i = 0; i < thickness; i++) {
+                g.drawRect(x + i, y + i,
+                           size - 1 - i * 2, size - 1 - i * 2);
+            }
+        }
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        selected = getPixelPosition(e.getPoint());
+        repaint();
+    }
+
+    private Position getPixelPosition(Point p) {
+        return new Position((int)(p.getX()) / getTileSize(),
+                            board.getWidth() - 1 -
+                            (int)(p.getY()) / getTileSize());
+    }
+
+    public void mouseExited(MouseEvent e) {
+        // Do nothing
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        // Do nothing
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        // Do nothing
+    }
+
+    public void mousePressed(MouseEvent e) {
+        // Do nothing
     }
 }

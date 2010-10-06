f6f1adf1a29553c4e19b3bb07892c57c89b97924
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-05 23:10:02 -0400

    Coded up the Queen.

--- src/com/nullprogram/chess/pieces/Queen.java
+++ src/com/nullprogram/chess/pieces/Queen.java
@@ -4,13 +4,23 @@ import com.nullprogram.chess.Piece;
 import com.nullprogram.chess.Position;
 import com.nullprogram.chess.PositionList;
 
+/**
+ * The Chess queen.
+ *
+ * This class describes the movement and capture behavior of the Chess
+ * queen.
+ */
 public class Queen extends Piece {
+
     public Queen(Side side) {
         super(side);
     }
 
     public PositionList getMoves() {
         PositionList list = new PositionList(getBoard());
+        // Take advantage of the Bishop and Rook implementations
+        list = Rook.getMoves(this, list);
+        list = Bishop.getMoves(this, list);
         return list;
     }
 }

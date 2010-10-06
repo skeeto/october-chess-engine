385157e7d8432fcb3f7aecfac8aadaf4c28b396a
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-05 20:57:10 -0400

    Flesh out Pawn with new Piece internals.

--- src/com/nullprogram/chess/pieces/Pawn.java
+++ src/com/nullprogram/chess/pieces/Pawn.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.nullprogram.chess.Position;
 import com.nullprogram.chess.Piece;
+import com.nullprogram.chess.Board;
 
 public class Pawn extends Piece {
     public Pawn(Side side) {
@@ -12,19 +13,32 @@ public class Pawn extends Piece {
 
     public ArrayList<Position> getMoves() {
         ArrayList<Position> list = new ArrayList<Position>();
+        Board b = getBoard();
         Position pos = getPosition();
-        Side side = getSide();
-        if (side == Side.WHITE) {
-            list.add(new Position(pos.x, pos.y + 1));
-            if (pos.x == 1) {
-                list.add(new Position(pos.x, pos.y + 2));
-            }
-        } else {
-            list.add(new Position(pos.x, pos.y - 1));
-            if (pos.x == 6) {
-                list.add(new Position(pos.x, pos.y - 2));
-            }
+        int dir = direction();
+        Position first = new Position(pos.x, pos.y + 1 * dir);
+        if (b.isEmpty(first) && b.inRange(first)) {
+            list.add(first);
+        }
+        Position second = new Position(pos.x, pos.y + 2 * dir);
+        if (!moved() && b.isEmpty(first) && b.inRange(first)) {
+            list.add(first);
         }
         return list;
+    }
+
+    private Boolean moved() {
+        Side side = getSide();
+        Position pos = getPosition();
+        return !((side == Side.WHITE && pos.x == 1) ||
+                 (side == Side.BLACK && pos.y == 6));
+    }
+
+    private int direction() {
+        if (getSide() == Side.WHITE) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }

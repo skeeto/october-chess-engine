41e4a36da39bdf8fb375543fb1ebd591083ca6e8
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-05 22:37:18 -0400

    Code up the King.

--- src/com/nullprogram/chess/pieces/King.java
+++ src/com/nullprogram/chess/pieces/King.java
@@ -4,6 +4,14 @@ import com.nullprogram.chess.Piece;
 import com.nullprogram.chess.Position;
 import com.nullprogram.chess.PositionList;
 
+/**
+ * The Chess king.
+ *
+ * This class describes the movement and capture behavior of the Chess
+ * king.
+ *
+ * TODO: check
+ */
 public class King extends Piece {
     public King(Side side) {
         super(side);
@@ -11,6 +19,14 @@ public class King extends Piece {
 
     public PositionList getMoves() {
         PositionList list = new PositionList(getBoard());
+        Position pos = getPosition();
+        for (int y = -1; y <= 1; y++) {
+            for (int x = -1; x <= 1; x++) {
+                if (x != 0 || y != 0) {
+                    list.addMove(new Position(pos, x, y), getSide());
+                }
+            }
+        }
         return list;
     }
 }

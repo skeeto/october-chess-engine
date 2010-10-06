f7969d31582d60dc98e345b24afa3fbedd3dea0b
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-05 22:41:09 -0400

    Code up the Knight.

--- src/com/nullprogram/chess/pieces/Knight.java
+++ src/com/nullprogram/chess/pieces/Knight.java
@@ -4,13 +4,29 @@ import com.nullprogram.chess.Piece;
 import com.nullprogram.chess.Position;
 import com.nullprogram.chess.PositionList;
 
+/**
+ * The Chess knight.
+ *
+ * This class describes the movement and capture behavior of the Chess
+ * knight.
+ */
 public class Knight extends Piece {
+
     public Knight(Side side) {
         super(side);
     }
 
     public PositionList getMoves() {
         PositionList list = new PositionList(getBoard());
+        Position pos = getPosition();
+        list.addMove(new Position(pos,  1,  2), getSide());
+        list.addMove(new Position(pos,  2,  1), getSide());
+        list.addMove(new Position(pos, -2,  1), getSide());
+        list.addMove(new Position(pos, -2, -1), getSide());
+        list.addMove(new Position(pos,  2, -1), getSide());
+        list.addMove(new Position(pos,  1, -2), getSide());
+        list.addMove(new Position(pos, -1, -2), getSide());
+        list.addMove(new Position(pos, -1,  2), getSide());
         return list;
     }
 }

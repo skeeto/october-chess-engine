5198022df8f2940953152fdae065da2dbf96e109
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-09 21:27:31 -0400

    Switch to anti-aliasing on new StatusBar.

--- src/com/nullprogram/chess/gui/StatusBar.java
+++ src/com/nullprogram/chess/gui/StatusBar.java
@@ -1,10 +1,12 @@
 package com.nullprogram.chess.gui;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Dimension;
 import java.awt.FontMetrics;
 import java.awt.Font;
 import java.awt.Color;
+import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
@@ -120,6 +122,10 @@ public class StatusBar extends JPanel {
     /** {@inheritDoc} */
     public final void paintComponent(final Graphics g) {
         super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+
         FontMetrics fm = g.getFontMetrics();
         int fh = fm.getAscent();
 

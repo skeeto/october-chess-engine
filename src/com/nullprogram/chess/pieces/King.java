b2e77f52867265764cc818e4a3a7ad2252ad3f0f
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-10 23:50:26 -0400

    Fully correct castling across all boards.

--- src/com/nullprogram/chess/pieces/King.java
+++ src/com/nullprogram/chess/pieces/King.java
@@ -1,7 +1,6 @@
 package com.nullprogram.chess.pieces;
 
 import com.nullprogram.chess.Piece;
-import com.nullprogram.chess.Board;
 import com.nullprogram.chess.Position;
 import com.nullprogram.chess.Move;
 import com.nullprogram.chess.MoveList;
@@ -14,20 +13,11 @@ import com.nullprogram.chess.MoveList;
  */
 public class King extends Piece {
 
-    /** Queen side knight column. */
-    static final int Q_KNIGHT = 1;
+    /** List of enemy moves (cahced). */
+    private MoveList enemy;
 
-    /** Queen side bishop column. */
-    static final int Q_BISHOP = 2;
-
-    /** Queen column. */
-    static final int QUEEN = 3;
-
-    /** King side bishop column. */
-    static final int K_BISHOP = 5;
-
-    /** King side knight column. */
-    static final int K_KNIGHT = 6;
+    /** Cache the check check. */
+    private Boolean inCheck;
 
     /**
      * Create a new king on the given side.
@@ -41,7 +31,6 @@ public class King extends Piece {
     /** {@inheritDoc} */
     public final MoveList getMoves(final boolean check) {
         MoveList list = new MoveList(getBoard(), check);
-        Boolean inCheck = null;  // expensive test: do it only 0 or 1 times!
         Position pos = getPosition();
         for (int y = -1; y <= 1; y++) {
             for (int x = -1; x <= 1; x++) {
@@ -52,48 +41,99 @@ public class King extends Piece {
         }
 
         /* check for castling */
-        if (!moved()) {
-            Board board = getBoard();
-            int home;
-            if (getSide() == Side.WHITE) {
-                home = 0;
-            } else {
-                home = board.getHeight() - 1;
+        enemy = null;
+        inCheck = null;
+        if (check && !moved()) {
+            Move left = castle(-1);
+            if (left != null) {
+                list.add(left);
             }
-            Position qRookPos = new Position(0, home);
-            Piece qRook = board.getPiece(qRookPos);
-            if (qRook != null
-                    && board.getPiece(qRookPos) instanceof Rook
-                    && !board.getPiece(qRookPos).moved()
-                    && (board.getPiece(new Position(Q_KNIGHT, home)) == null)
-                    && (board.getPiece(new Position(Q_BISHOP, home)) == null)
-                    && (board.getPiece(new Position(QUEEN, home)) == null)) {
-                /* castle queen-side */
-                inCheck = check && getBoard().check(getSide());
-                if (!inCheck) {
-                    Move king = new Move(pos, new Position(pos, -1 * 2, 0));
-                    king.setNext(new Move(qRookPos, new Position(pos, -1, 0)));
-                    list.add(king);
-                }
-            }
-            Position kRookPos = new Position(board.getWidth() - 1, home);
-            Piece kRook = board.getPiece(kRookPos);
-            if (kRook != null
-                    && board.getPiece(kRookPos) instanceof Rook
-                    && !board.getPiece(kRookPos).moved()
-                    && (board.getPiece(new Position(K_KNIGHT, home)) == null)
-                    && (board.getPiece(new Position(K_BISHOP, home)) == null)) {
-                /* castle king-side */
-                if (inCheck == null) {
-                    inCheck = check && getBoard().check(getSide());
-                }
-                if (!inCheck) {
-                    Move king = new Move(pos, new Position(pos, 2, 0));
-                    king.setNext(new Move(kRookPos, new Position(pos, 1, 0)));
-                    list.add(king);
-                }
+            Move right = castle(1);
+            if (right != null) {
+                list.add(right);
             }
         }
         return list;
+    }
+
+    /**
+     * Try to create a castle move in the given direction.
+     *
+     * @param dir direction to check
+     * @return the move, or null
+     */
+    private Move castle(final int dir) {
+        int dist = getBoard().getWidth() / 2 - 2;
+        Position pos = getPosition();
+
+        int max;
+        if (dir < 0) {
+            max = 0;
+        } else {
+            max = getBoard().getWidth() - 1;
+        }
+
+        Position rookPos = new Position(max, pos.getY());
+        Piece rook = getBoard().getPiece(rookPos);
+        if (rook != null && rook.moved()) {
+            return null;
+        }
+
+        if (emptyRow(getPosition(), dir, max) && !inCheck()) {
+            /* generate the move */
+            Position kpos = new Position(pos, dir * dist, 0);
+            Move kingDest = new Move(pos, kpos);
+            Position rpos = new Position(pos, dir * dist - dir, 0);
+            Move rookDest = new Move(rookPos, rpos);
+            kingDest.setNext(rookDest);
+            return kingDest;
+        }
+        return null;
+    }
+
+    /**
+     * Check for an empty, unthreatened castling row.
+     *
+     * @param start the starting position
+     * @param dir direction to check
+     * @param max maximum column for the board
+     * @return true if row is safe
+     */
+    private boolean emptyRow(final Position start, final int dir,
+                             final int max) {
+        for (int i = start.getX() + dir; i != max; i += dir) {
+            Position pos = new Position(i, start.getY());
+            if (getBoard().getPiece(pos) != null
+                    || enemyMoves().containsDest(pos)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Cache of enemy moves.
+     *
+     * @return list of enemy moves
+     */
+    private MoveList enemyMoves() {
+        if (enemy != null) {
+            return enemy;
+        }
+        enemy = getBoard().allMoves(opposite(getSide()), false);
+        return enemy;
+    }
+
+    /**
+     * Cache of check check.
+     *
+     * @return true if king is in check
+     */
+    private boolean inCheck() {
+        if (inCheck != null) {
+            return inCheck;
+        }
+        inCheck = getBoard().check(getSide());
+        return inCheck;
     }
 }

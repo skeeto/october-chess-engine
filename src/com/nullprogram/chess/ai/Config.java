bf082b03515112e3a08007f2b015ad694ffa36a8
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-13 16:49:41 -0400

    Begin work on GA parameter optimizer.

---
+++ src/com/nullprogram/chess/ai/Config.java
@@ -0,0 +1,49 @@
+package com.nullprogram.chess.ai;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+/**
+ * Represents a particular configuration of an AI player.
+ */
+public class Config extends HashMap<String, Double> {
+
+    /** List of all currently existing properties. */
+    static final String[] PLIST = {"depth", "Pawn", "Knight", "Bishop", "Rook",
+                                   "Queen", "King", "Chancellor", "Archbishop",
+                                   "material", "safety", "mobility"};
+
+    /** Version for object serialization. */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create a new configuration.
+     */
+    public Config() {
+        super();
+    }
+
+    /**
+     * Create a config from a set of properties.
+     *
+     * @param props the proprties to derive from
+     */
+    public Config(final Properties props) {
+        for (String prop : PLIST) {
+            put(prop, Double.parseDouble(props.getProperty(prop)));
+        }
+    }
+
+    /**
+     * Create a properties object from tihs config.
+     *
+     * @return the properteies that matches this config
+     */
+    public final Properties getProperties() {
+        Properties props = new Properties();
+        for (String prop : PLIST) {
+            props.setProperty(prop, "" + get(prop));
+        }
+        return props;
+    }
+}
---
+++ src/com/nullprogram/chess/ai/OptimizeGA.java
@@ -0,0 +1,71 @@
+package com.nullprogram.chess.ai;
+
+import java.util.Random;
+
+import com.nullprogram.chess.Game;
+import com.nullprogram.chess.Board;
+import com.nullprogram.chess.Player;
+
+import com.nullprogram.chess.boards.StandardBoard;
+
+/**
+ * Alternte main class optimizing AI parameters via genetic algorithm.
+ */
+public class OptimizeGA {
+
+    /** Random number generator.  */
+    private static Random rng;
+
+    /** Maximum range for piece values. */
+    static final double PIECE_RANGE = 10.0;
+
+    /** Locked-in dpeth: we don't change this in the population. */
+    static final int DEPTH = 4;
+
+    /**
+     * Hidden constructor.
+     */
+    protected OptimizeGA() {
+    }
+
+    /**
+     * Rather than start the GUI, run undisplayed games.
+     *
+     * @param args input arguments
+     */
+    public static void main(final String[] args) {
+        rng = new Random();
+
+        Config whiteConf = create();
+        Config blackConf = create();
+        Player white = new Minimax(null, whiteConf.getProperties());
+        Player black = new Minimax(null, whiteConf.getProperties());
+        Board board = new StandardBoard();
+        Game game = new Game(null, board, white, black);
+        game.begin();
+    }
+
+    /**
+     * Randomly create a new configuration.
+     *
+     * @return a randomly generated config
+     */
+    private static Config create() {
+        Config conf = new Config();
+        conf.put("depth", (double) DEPTH);
+        String[] pieces = {"Pawn", "Knight", "Bishop", "Rook", "Queen",
+                           "King", "Chancellor", "Archbishop"};
+        for (String piece : pieces) {
+            Double v = rng.nextDouble() * PIECE_RANGE;
+            if ("King".equals(piece)) {
+                /* The king has a much larger value range. */
+                v *= PIECE_RANGE * PIECE_RANGE;
+            }
+            conf.put(piece, v);
+        }
+        conf.put("material", rng.nextDouble());
+        conf.put("safety", rng.nextDouble());
+        conf.put("mobility", rng.nextDouble());
+        return conf;
+    }
+}

b05ba0315f63517406b6240eb0223dcd7145f997
Christopher Wellons <mosquitopsu@gmail.com>
2010-10-17 12:18:17 -0400

    Make the applet a game listener so it can reveal how the game ended.

--- src/com/nullprogram/chess/ChessApplet.java
+++ src/com/nullprogram/chess/ChessApplet.java
@@ -18,6 +18,7 @@ package com.nullprogram.chess;
 
 import javax.swing.UIManager;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 import com.nullprogram.chess.gui.BoardPanel;
 
@@ -28,7 +29,7 @@ import com.nullprogram.chess.ai.Minimax;
 /**
  * Applet that runs a game versus the computer, no other options.
  */
-public final class ChessApplet extends JApplet {
+public final class ChessApplet extends JApplet implements GameListener {
 
     /** Version for object serialization. */
     private static final long serialVersionUID = 34863129470926196L;
@@ -46,6 +47,23 @@ public final class ChessApplet extends JApplet {
         BoardPanel panel = new BoardPanel(board);
         add(panel);
         Game game = new Game(null, board, panel, new Minimax(null));
+        game.addListener(this);
         game.begin();
+    }
+
+    /** {@inheritDoc} */
+    public void gameEvent(final Game game) {
+        if (game.isDone()) {
+            String message;
+            Piece.Side winner = game.getWinner();
+            if (winner == Piece.Side.WHITE) {
+                message = "White wins";
+            } else if (winner == Piece.Side.BLACK) {
+                message = "Black wins";
+            } else {
+                message = "Stalemate";
+            }
+            JOptionPane.showMessageDialog(null, message);
+        }
     }
 }

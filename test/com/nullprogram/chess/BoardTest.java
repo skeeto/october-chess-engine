fabcffaaee440152403e9240b83ad5a784c1107a
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-08 22:52:30 -0500

    Update the unit test.

--- test/com/nullprogram/chess/BoardTest.java
+++ test/com/nullprogram/chess/BoardTest.java
@@ -1,25 +1,20 @@
 package com.nullprogram.chess;
 
-import junit.framework.TestCase;
 import com.nullprogram.chess.boards.EmptyBoard;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class BoardTest extends TestCase {
+public class BoardTest {
 
-    Board board;
-    static final int SIZE = 8;
+    private static final int SIZE = 8;
+    private static Board board = new EmptyBoard(SIZE, SIZE);
 
-    public BoardTest(String name) {
-        super(name);
-    }
-
-    public void setUp() {
-        board = new EmptyBoard(SIZE, SIZE);
-    }
-
+    @Test
     public void testGetWidth() {
         assertEquals(SIZE, board.getWidth());
     }
 
+    @Test
     public void testGetHeight() {
         assertEquals(SIZE, board.getWidth());
     }
